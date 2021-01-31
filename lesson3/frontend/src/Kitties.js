146a0e76bd3f421509cd965c827d173b82dee604
klcstudy <klc126@126.com>
2021-01-31 11:10:19 +0800

    homework for lesson3

---
+++ lesson3/frontend/src/Kitties.js
@@ -0,0 +1,92 @@
+import React, {useEffect, useState} from 'react';
+import {Form, Grid} from 'semantic-ui-react';
+
+import {useSubstrate} from './substrate-lib';
+import {TxButton} from './substrate-lib/components';
+
+import KittyCards from './KittyCards';
+
+export default function Kitties (props) {
+  const { api, keyring } = useSubstrate();
+  const { accountPair } = props;
+
+  const [kittyCnt, setKittyCnt] = useState(0);
+  const [kittyDNAs, setKittyDNAs] = useState([]);
+  const [kittyOwners, setKittyOwners] = useState([]);
+  const [kittyPrices, setKittyPrices] = useState([]);
+  const [kitties, setKitties] = useState([]);
+  const [status, setStatus] = useState('');
+
+  const fetchKittyCnt = () => {
+    let unsubscribe;
+    api.query.kittiesModule.kittiesCount(count => {
+      setKittyCnt(count.toNumber());
+    }).then(unsub => {
+      unsubscribe = unsub;
+    }).catch(console.error);
+    return () => unsubscribe && unsubscribe();
+  };
+
+  const fetchKitties = () => {
+    let unsubscribe;
+    let indexs = [];
+    for (let i = 0; i < kittyCnt; i++) {
+      indexs.push(i);
+    }
+    api.query.kittiesModule.kitties.multi(indexs, (data) => {
+      const items = data.map((dna, id) => {
+        return {
+          dna,
+          id,
+        }
+      })
+      setKitties(items);
+    }).then(unsub => {
+      unsubscribe = unsub;
+    }).catch(console.error);
+    return () => unsubscribe && unsubscribe();
+  };
+
+  const fetchKittyOwners = () => {
+    let unsubscribe;
+    let indexs = [];
+    for (let i = 0; i < kittyCnt; i++) {
+      indexs.push(i);
+    }
+    api.query.kittiesModule.kittyOwners.multi(indexs, (data) => {
+      setKittyOwners(data);
+    }).then(unsub => {
+      unsubscribe = unsub;
+    }).catch(console.error);
+    return () => unsubscribe && unsubscribe();
+  };
+
+  const populateKitties = () => {
+    /* TODO: 加代码，从 substrate 端读取数据过来 */ 
+  };
+
+  useEffect(fetchKittyCnt, [api, keyring, setKittyCnt, api.query.kittiesModule]);
+  useEffect(fetchKitties, [api, kittyCnt, setKitties, api.query.kittiesModule]);
+  useEffect(fetchKittyOwners, [api, kittyCnt, setKittyOwners, api.query.kittiesModule]);
+  useEffect(populateKitties, [kittyDNAs, kittyOwners]);
+
+  return <Grid.Column width={16}>
+    <h1>小毛孩</h1>
+    <KittyCards kitties={kitties} kittyOwners={kittyOwners} kittyPrices={kittyPrices} accountPair={accountPair} setStatus={setStatus}/>
+    <Form style={{ margin: '1em 0' }}>
+      <Form.Field style={{ textAlign: 'center' }}>
+        <TxButton
+          accountPair={accountPair} label='创建小毛孩' type='SIGNED-TX' setStatus={setStatus}
+          attrs={{
+            palletRpc: 'kittiesModule',
+            callable: 'create',
+            inputParams: [],
+            paramFields: []
+          }}
+        />
+      </Form.Field>
+    </Form>
+    <p>总共有个{kittyCnt}毛孩</p>
+    <div style={{ overflowWrap: 'break-word' }}>{status}</div>
+  </Grid.Column>;
+}
